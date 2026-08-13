ac4d6f3fbb2c681bd06cf0fbf9512a3ebcd33b5f
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-08-13 21:47:50 +0530

    deployment changes

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ const startServer = async () => {
       console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
-    console.error("Server error:", error.message);
+    console.error("Server error:", error);
     process.exit(1);
   }
 };
